782ce135a2c6485e675eabf56b457e9838d4bec9
María Camila Guerrero Giraldo <macaguegi@yahoo.es>
2020-07-29 20:52:17 -0500

    Se mezclan los cambios entre logs y templates para mostrar los personajes y generar logs

--- main.go
+++ main.go
@@ -10,10 +10,31 @@ import (
 	"time"
 )
 
-func main() {
-	// Benchmarking of the request time
-	now := time.Now()
+// Benchmarking of the request time
+var now = time.Now()
 
+func main() {
+	http.HandleFunc("/", CharactersListHandler)
+	http.ListenAndServe(":8000", nil)
+}
+
+func CharactersListHandler(w http.ResponseWriter, r *http.Request) {
+	results, err := rickAndMortyApi.GetAllCharacters()
+	LogCharacters(results)
+	tmpl, err := template.New("").ParseFiles("rickAndMortyApi/templates/characters.html", "rickAndMortyApi/templates/base.html")
+
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.ExecuteTemplate(w, "base", results)
+
+	if err != nil {
+		log.Fatalf("error while getting all characters: %v", err)
+	}
+
+}
+
+func LogCharacters(results []rickAndMortyApi.Character) () {
 	rosterFile, err := os.OpenFile("roasters.txt", os.O_RDWR|os.O_CREATE|os.O_CREATE, 0666)
 
 	if err != nil {
@@ -25,24 +46,10 @@ func main() {
 	// log at the same time in terminal and in the roasterFile
 	wrt := io.MultiWriter(os.Stdout, rosterFile)
 	log.SetOutput(wrt)
-
-	http.HandleFunc("/", CharactersListHandler)
-	http.ListenAndServe(":8000", nil)
-
+	for _, character := range results {
+		log.Println("------------------------------")
+		log.Printf("Name %s", character.Name)
+		log.Println("------------------------------")
+	}
 	log.Printf("took %v", time.Now().Sub(now).String())
-}
-
-func CharactersListHandler(w http.ResponseWriter, r *http.Request) {
-	results, err := rickAndMortyApi.GetAllCharacters()
-
-	tmpl, err := template.New("").ParseFiles("rickAndMortyApi/templates/characters.html", "rickAndMortyApi/templates/base.html")
-	// check your err
-	if err != nil {
-		panic(err)
-	}
-	err = tmpl.ExecuteTemplate(w, "base", results)
-
-	if err != nil {
-		log.Fatalf("error while getting all characters: %v", err)
-	}
 }
